c5a4e3e82a0f8f5764bbda5044808917196f8dd8
Lukas Stappen <lukas.stappen@kcl.ac.uk>
2017-07-07 16:26:01 +0000

    Update ServoConfig.py
    Implement Lower/UPPER boundaries to avoid spinning servo motors

--- ServoConfig.py
+++ ServoConfig.py
@@ -3,7 +3,6 @@ import sys
 import RPi.GPIO as GPIO
 import Adafruit_PCA9685
 
-
 def BUSInit():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(11, GPIO.OUT)
@@ -22,7 +21,7 @@ def Writer(pin,position,var):
             conf.write("[PIN %s]: Angle: %s\r\n"%(pin,var[1]))
             conf.close() #not sure if necessary
     except IOError as ie:
-        print "Open file not possible: %s"%ie
+        print ("Open file not possible: %s"%ie)
 
 #Adjust the configuration of puls modulation
 #Idea:      iterate through most likely positions automatically
@@ -30,42 +29,49 @@ def Writer(pin,position,var):
 #           correct position, afterwards fine tuning
 def AngleConfiguration(pin, position):
     Bool = False
+    servoold = None
     while Bool == False:
         servo = input("[PIN %s]:Input angle: "%pin)
-
-        print ("Testoutput" + servo)
-        if servo == "":
+        
+        if servo == "" and servoold is not None:
             Bool = True
+            #GPIO.cleanup()
             servoangle = input("Please give the resultating angle (degrees)")            
             Writer(pin,position,(servoold,servoangle))            
         else:
-            pwm.set_pwm(pin,0,int(servo))
-            servoold = servo
+            if servo != "":
+                if int(servo) > LOWER_BOUND and int(servo) < UPPER_BOUND:
+                    pwm.set_pwm(pin,0,int(servo))
+                    servoold = servo
+                else:
+                    print("The min value is %d and the max value is %d"%(LOWER_BOUND,UPPER_BOUND))
+            else:
+                print("At least one entry is necessary!")
 
 #Connect to servomotor of connected pins from pi to and.
 #Idea:  Dynamic by parameter 
 def PinConfiguration():
     try:
         with open(file, 'a+') as conf: #filehandle creates file now if not existing
-        conf.close()
+            conf.close()
             try:       
-                    #PIN0
-                    outputpin = 0
-                    #CallBoundarySetting
-                    print("[PIN %s]: Maximum angle configuration(around 500)"%outputpin)
-                    AngleConfiguration(outputpin,"max")
-                    print("[PIN %s]: Middle angle configuration(around 350)"%outputpin)
-                    AngleConfiguration(outputpin,"middle")                          
-                    print("[PIN %s]: Minimum angle configuration(around 150)"%outputpin)
-                    AngleConfiguration(outputpin,"min")
-                    #PIN1
-                    outputpin = 1
-                    print("[PIN %s]: Maximum angle configuration(around 500)"%outputpin)
-                    AngleConfiguration(outputpin,"max")
-                    print("[PIN %s]: Middle angle configuration(around 350)"%outputpin)
-                    AngleConfiguration(outputpin,"middle")                          
-                    print("[PIN %s]: Minimum angle configuration(around 150)"%outputpin)
-                    AngleConfiguration(outputpin,"min")                       
+                #PIN0
+                outputpin = 0
+                #CallBoundarySetting
+                print("[PIN %s]: Maximum angle configuration(around 500)"%outputpin)
+                AngleConfiguration(outputpin,"max")
+                print("[PIN %s]: Middle angle configuration(around 350)"%outputpin)
+                AngleConfiguration(outputpin,"middle")                          
+                print("[PIN %s]: Minimum angle configuration(around 150)"%outputpin)
+                AngleConfiguration(outputpin,"min")
+                #PIN1
+                outputpin = 1
+                print("[PIN %s]: Maximum angle configuration(around 500)"%outputpin)
+                AngleConfiguration(outputpin,"max")
+                print("[PIN %s]: Middle angle configuration(around 350)"%outputpin)
+                AngleConfiguration(outputpin,"middle")                          
+                print("[PIN %s]: Minimum angle configuration(around 150)"%outputpin)
+                AngleConfiguration(outputpin,"min")                       
                 print("Configuration complete!")
                 GPIO.output(11,0)
                 GPIO.cleanup()                
@@ -76,15 +82,15 @@ def PinConfiguration():
                 conf.close()
                 sys.exit()
     except IOError:
-        print "Open file not possible"
+        print("Open file not possible")
 
-if __name__ == '__main__':
+if __name__ == '__main__':  
     #define bushandle for all            
-    pwm = BUSInit()    
+    pwm = BUSInit()  
     #name of file or parameter input
     file = 'ServoConfig.txt'
     #define constants to avoid crashing the servomotors
-    UPPER_BOUND = 180
-    LOWER_BOUND = 300
+    UPPER_BOUND = 400
+    LOWER_BOUND = 180
     
     PinConfiguration()
